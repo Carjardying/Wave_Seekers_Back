e9c8aa4a354f4808a72e910cf1bc5f44bccc5e7a
Flaya <delphine.jozeau@gmail.com>
2025-09-08 12:34:01 +0200

    update: route get spots by user Id

--- Models/spot.go
+++ Models/spot.go
@@ -112,3 +112,24 @@ func GetSpotsByCountryID(db *sql.DB, countryID int) ([]Spot, error) {
 	}
 	return spots, nil
 }
+
+//Select spot by user id
+func GetSpotsByUserID(db *sql.DB, userID int) ([]Spot, error) {
+	rows, err := db.Query(`SELECT id, user_id, country_id, destination, location, lat, long, peak_season_start, peak_season_end, difficulty_level, surfing_culture, image_url FROM spot WHERE user_id = ?`, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var spots []Spot
+	for rows.Next() {
+		var spot Spot
+		err := rows.Scan(&spot.ID, &spot.UserID, &spot.CountryID, &spot.Destination, &spot.Location, &spot.Lat, &spot.Long, &spot.PeakSeasonStart, &spot.PeakSeasonEnd, &spot.DifficultyLevel, &spot.SurfingCulture, &spot.ImageURL)
+		if err != nil {
+			return nil, err
+		}
+		//la table spots = append(la table spots, le spot)
+		spots = append(spots, spot)
+	}
+	return spots, nil
+}
--- main.go
+++ main.go
@@ -65,6 +65,7 @@ func main() {
 	router.GET("/spots", getAllSpotsHandler)
 	router.GET("/spots/:id", getSpotByIDHandler) //Spot's Details
 	router.GET("/spots/country/:country_id", getSpotByCountryHandler)
+	router.GET("/spots/user/:user_id", getSpotsByUserIDHandler)
 
 	router.POST("/spots", addSpotHandler)
 
@@ -155,6 +156,29 @@ func getSpotByCountryHandler(c *gin.Context) {
 		return
 	}
 	c.IndentedJSON(http.StatusOK, spot)
+}
+
+func getSpotsByUserIDHandler(c *gin.Context){
+	idStr := c.Param("user_id")
+
+	user_id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid spot ID"})
+		return
+	}
+
+	spot, err := Models.GetSpotsByUserID(db, user_id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "spot not found"})
+		} else {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "error fetching spot"})
+		}
+		return
+	}
+	c.IndentedJSON(http.StatusOK, spot)
+
 }
 
 /*---------- POST------*/
